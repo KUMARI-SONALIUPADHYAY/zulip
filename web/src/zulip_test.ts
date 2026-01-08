e163ad9cf5100b778321384c1be6158ed6559877
Tim Abbott <tabbott@zulip.com>
2026-01-08 10:46:37 -0800

    zulip_test: Export some emoji frequency properties.

--- web/src/zulip_test.ts
+++ web/src/zulip_test.ts
@@ -12,6 +12,8 @@ export {cancel as cancel_compose} from "./compose_actions.ts";
 export {page_params, page_params_parse_time} from "./base_page_params.ts";
 export {initiate as initiate_reload} from "./reload.ts";
 export {page_load_time} from "./setup.ts";
+export {reaction_data} from "./emoji_frequency.ts";
+export {frequently_used_emojis} from "./typeahead.ts";
 export {current_user, realm} from "./state_data.ts";
 export {add_user_id_to_new_stream} from "./stream_create_subscribers.ts";
 export {get as get_message} from "./message_store.ts";
